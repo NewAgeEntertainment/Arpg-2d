c350f0f90664b5b35bf9ff2486e0043fe95551b3
Javed Rehman <javedbarcha@gmail.com>
2025-06-08 01:01:53 +0500

    add enemy attack indicators

--- Assets/Scripts/Enemy/EnemyCombat.cs
+++ Assets/Scripts/Enemy/EnemyCombat.cs
@@ -4,66 +4,78 @@ using UnityEngine;
 
 public class EnemyCombat : Entity_Combat
 {
+    [SerializeField] private Transform _targetCheck_Left;
+    [SerializeField] private Transform _targetCheck_Right;
+    [SerializeField] private Transform _targetCheck_Up;
+    [SerializeField] private Transform _targetCheck_Down;
 
-    public Transform targetCheck;
-    public void FlipTargetCheck()
-    {
-        if (enemy == null)
-        {
-            enemy = GetComponent<Enemy>();
-        }
-
-        // Determine the direction based on currentDir and adjust targetCheck position accordingly  
-        if (enemy.currentDir.x > 0) // Facing right  
-        {
-            targetCheck.position = new Vector3(transform.position.x + 1, transform.position.y, transform.position.z);
-        }
-        else if (enemy.currentDir.x < 0) // Facing left  
-        {
-            targetCheck.position = new Vector3(transform.position.x - 1, transform.position.y, transform.position.z);
-        }
-        else if (enemy.currentDir.y > 0) // Facing up  
-        {
-            targetCheck.position = new Vector3(transform.position.x, transform.position.y + 1, transform.position.z);
-        }
-        else if (enemy.currentDir.y < 0) // Facing down  
-        {
-            targetCheck.position = new Vector3(transform.position.x, transform.position.y - 1, transform.position.z);
-        }
-    }
-
-    public void Update()
-    {
-        // Ensure enemy reference is assigned  
-        if (enemy == null)
-        {
-            enemy = GetComponent<Enemy>();
-        }
-
-        // Call FlipTargetCheck to update targetCheck position based on direction  
-        FlipTargetCheck();
-    }
     private Enemy enemy;
 
-    
+    private void Awake()
+    {
+        enemy = GetComponent<Enemy>();
+    }
 
     public override Collider2D[] GetDetectedCollider()
     {
         List<Collider2D> detected = new List<Collider2D>();
 
-        Collider2D[] colliders = Physics2D.OverlapCircleAll(targetCheck.position, targetCheckRadius, whatIsTarget);
+        // Get the appropriate target check based on enemy's direction
+        Transform targetCheck = GetTargetTransform();
+        if (targetCheck != null)
+        {
+            Collider2D[] colliders = Physics2D.OverlapCircleAll(targetCheck.position, targetCheckRadius, whatIsTarget);
+            detected.AddRange(colliders);
+        }
 
-        // Combine the detected colliders into the list  
-        detected.AddRange(colliders);
-
-        // Return the combined colliders as an array  
         return detected.ToArray();
+    }
+
+    private Transform GetTargetTransform()
+    {
+        if (enemy.currentDir.y >= 1)
+        {
+            return _targetCheck_Up;
+        }
+        if (enemy.currentDir.y <= -1)
+        {
+            return _targetCheck_Down;
+        }
+        if (enemy.currentDir.x <= -1)
+        {
+            return _targetCheck_Left;
+        }
+        return _targetCheck_Right;
     }
 
     private void OnDrawGizmos()
     {
-        // Draw a wireframe sphere to visualize the target detection radius
-        Gizmos.color = Color.red;
-        Gizmos.DrawWireSphere(targetCheck.position, targetCheckRadius);
+        Transform[] targetChecks = { _targetCheck_Left, _targetCheck_Right, _targetCheck_Up, _targetCheck_Down };
+
+        foreach (Transform check in targetChecks)
+        {
+            if (check != null)
+            {
+                // Check if there are any players in this direction's target check area
+                Collider2D[] colliders = Physics2D.OverlapCircleAll(check.position, targetCheckRadius, whatIsTarget);
+                
+                // Only show the gizmo if there are players detected in this direction
+                if (colliders.Length > 0)
+                {
+                    // Check if any of the detected colliders are the player
+                    foreach (Collider2D collider in colliders)
+                    {
+                        if (collider.CompareTag("Player"))
+                        {
+                            // Draw the target check sphere and line only if player is detected
+                            Gizmos.color = Color.red;
+                            Gizmos.DrawWireSphere(check.position, targetCheckRadius);
+                            Gizmos.DrawLine(transform.position, check.position);
+                            break; // Exit the loop once we find the player
+                        }
+                    }
+                }
+            }
+        }
     }
 }
