af554276363b5d3c5bb92ecf7dc95a1549810902
NewAgeEntertainment <114917721+NewAgeEntertainment@users.noreply.github.com>
2025-08-27 20:27:26 -0700

    found my resolution

--- Assets/Scripts/Companion/Ping2D.cs
+++
@@ -1,8 +0,0 @@
-using UnityEngine;
-public class Ping2D : MonoBehaviour
-{
-    private void OnTriggerEnter2D(Collider2D other)
-    {
-        Debug.Log($"[Ping2D] {name} got enter from {other.name}");
-    }
-}
