49eafdfb93c5fe157637cf212f603f9b88df71e5
NewAgeEntertainment <114917721+NewAgeEntertainment@users.noreply.github.com>
2025-09-06 22:56:33 -0700

    finalized levels

---
+++ Assets/Scripts/AttachAnimator.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class AttachAnimator : MonoBehaviour
+{
+    public PlayableDirector playableDirector;
+    public int trackIndex;
+    Animator animator;
+
+    void Awake()
+    {
+        GameObject player = GameObject.FindGameObjectWithTag("Player");
+        int index = 0;
+        if (player != null)
+        {
+            animator = player.GetComponent<Animator>();
+        }
+        TimelineAsset timelineAsset = playableDirector.playableAsset as TimelineAsset;
+
+        IEnumerable<TrackAsset> trackList = timelineAsset.GetOutputTracks();
+
+        foreach (TrackAsset trackAsset in trackList)
+        {
+            // check to see if this is the one you are looking for (by name, index etc)
+            if (index == trackIndex)
+            {
+                // bind the track to our new actor instance
+                playableDirector.SetGenericBinding(trackAsset, animator);
+                break;
+            }
+
+            index += 1;
+        }
+    }
+}
---
+++ Assets/Scripts/AttachModelAnimator.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Playables;
+using UnityEngine.Timeline;
+
+public class AttachModelAnimator : MonoBehaviour
+{
+    public PlayableDirector playableDirector;
+    public int trackIndex;
+    Animator animator;
+
+    void Awake()
+    {
+        GameObject model = GameObject.FindGameObjectWithTag("Model");
+        int index = 0;
+        if (model != null)
+        {
+            animator = model.GetComponent<Animator>();
+        }
+        TimelineAsset timelineAsset = playableDirector.playableAsset as TimelineAsset;
+
+        IEnumerable<TrackAsset> trackList = timelineAsset.GetOutputTracks();
+
+        foreach (TrackAsset trackAsset in trackList)
+        {
+            // check to see if this is the one you are looking for (by name, index etc)
+            if (index == trackIndex)
+            {
+                // bind the track to our new actor instance
+                playableDirector.SetGenericBinding(trackAsset, animator);
+                break;
+            }
+
+            index += 1;
+        }
+    }
+}
